ca4cce7db4c8ad42e8811220eca53a2b2634e70e
alf3ratz <49417479+alf3ratz@users.noreply.github.com>
2021-04-17 18:19:36 +0300

    add search activity_search.xml

--- app/src/main/java/course/ru/qsearcher/viewmodels/SearchViewModel.kt
+++ app/src/main/java/course/ru/qsearcher/viewmodels/SearchViewModel.kt
@@ -1,4 +1,20 @@
 package course.ru.qsearcher.viewmodels
 
-class SearchViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import course.ru.qsearcher.repositories.SearchEventRepository
+import course.ru.qsearcher.responses.EventResponse
+
+class SearchViewModel() : ViewModel() {
+
+    private var searchEventRepository: SearchEventRepository? = null
+
+    init {
+        searchEventRepository = SearchEventRepository()
+    }
+
+    fun searchEvent(query: String, page: Int): LiveData<EventResponse> {
+        return searchEventRepository?.searchEvent(query, page)!!
+    }
+
 }
