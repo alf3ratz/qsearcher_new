7c493d9548a9216f2174e2dabbb3d1a106bcb8c7
alf3ratz <49417479+alf3ratz@users.noreply.github.com>
2021-04-23 18:06:30 +0300

    start making signIn #7

--- app/src/main/java/course/ru/qsearcher/activities/ChatActivity.kt
+++ app/src/main/java/course/ru/qsearcher/activities/ChatActivity.kt
@@ -1,6 +1,7 @@
 package course.ru.qsearcher.activities
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -15,6 +16,9 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 import course.ru.qsearcher.R
 import course.ru.qsearcher.adapters.MessageAdapter
 import course.ru.qsearcher.databinding.ActivityChatBinding
@@ -24,17 +28,21 @@ import course.ru.qsearcher.model.User
 import kotlinx.android.synthetic.main.activity_chat.*
 
 class ChatActivity : AppCompatActivity() {
+    private val RC_IMAGE: Int = 1
 
     private var messageAdapter: MessageAdapter? = null
     private var database: FirebaseDatabase? = null
     private var messagesRef: DatabaseReference? = null
     private var usersRef: DatabaseReference? = null
-    private var usersChildEventListener:ChildEventListener?=null
+    private var usersChildEventListener: ChildEventListener? = null
     private var userName: String? = null
 
     private var activityChatBinding: ActivityChatBinding? = null
 
     private var messagesChildEventListener: ChildEventListener? = null
+    private var storage: FirebaseStorage? = null
+    private var storageRef: StorageReference? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,10 +50,10 @@ class ChatActivity : AppCompatActivity() {
         database = Firebase.database
         messagesRef = database!!.reference.child("message")
         usersRef = database!!.reference.child("users")
-        usersChildEventListener = object:ChildEventListener{
+        usersChildEventListener = object : ChildEventListener {
             override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
-                var user:User = snapshot.getValue(User::class.java)!!
-                if(user.id == FirebaseAuth.getInstance().currentUser.uid){
+                var user: User = snapshot.getValue(User::class.java)!!
+                if (user.id == FirebaseAuth.getInstance().currentUser.uid) {
                     userName = user.name
                 }
 
@@ -96,7 +104,12 @@ class ChatActivity : AppCompatActivity() {
         })
         Log.i("db", "дошел")
         messageEdit?.filters = arrayOf(InputFilter.LengthFilter(500))
-        sendPhotoButton?.setOnClickListener { messageEdit?.setText("") }
+        sendPhotoButton?.setOnClickListener {
+            var intent: Intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true)
+            startActivityForResult(Intent.createChooser(intent, "Выберите изображение"), RC_IMAGE)
+        }
         imageBack.setOnClickListener { onBackPressed() }
         sendMessageButton.setOnClickListener {
 
@@ -136,6 +149,22 @@ class ChatActivity : AppCompatActivity() {
         imageSignOut?.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
             startActivity(Intent(applicationContext, SignInActivity::class.java))
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_IMAGE && resultCode == RESULT_OK) {
+            var selectedImage: Uri = data?.data!!
+            var imgRef: StorageReference =
+                selectedImage.lastPathSegment?.let { storageRef?.child(it) }!!
+            var uploadTask:UploadTask = imgRef.putFile(selectedImage)
+            uploadTask.addOnFailureListener {
+                // Handle unsuccessful uploads
+            }.addOnSuccessListener { taskSnapshot ->
+                // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+                // ...
+            }
         }
     }
 }
