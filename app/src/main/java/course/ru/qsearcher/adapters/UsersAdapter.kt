1d1d1bb30ce393cb3b392dff1b26d90771fa6de9
alf3ratz <49417479+alf3ratz@users.noreply.github.com>
2021-04-24 00:49:56 +0300

    add recycler for messages #2

--- app/src/main/java/course/ru/qsearcher/adapters/UsersAdapter.kt
+++ app/src/main/java/course/ru/qsearcher/adapters/UsersAdapter.kt
@@ -1,4 +1,32 @@
 package course.ru.qsearcher.adapters
 
-class UsersAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import course.ru.qsearcher.databinding.UserItemBinding
+import course.ru.qsearcher.model.User
+
+class UsersAdapter : RecyclerView.Adapter<UsersAdapter.UsersViewHolder>() {
+    private lateinit var users: MutableList<User>
+
+
+    inner class UsersViewHolder(itemLayoutBinding: UserItemBinding) :
+        RecyclerView.ViewHolder(itemLayoutBinding.root) {
+
+
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): UsersAdapter.UsersViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: UsersAdapter.UsersViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
